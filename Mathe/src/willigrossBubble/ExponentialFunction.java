5a4e08eb7f107e44caa729e123a2109c643ad293
WilliGross <dergrossewilligross@gmail.com>
2016-02-02 21:50:10 +0100

    Improved display of function terms

--- Mathe/src/willigrossBubble/ExponentialFunction.java
+++ Mathe/src/willigrossBubble/ExponentialFunction.java
@@ -2,7 +2,6 @@ package willigrossBubble;
 
 public class ExponentialFunction extends Function {
 
-
 	/**
 	 * Create a function whose graph runs through two given points
 	 * @param p - point 1
@@ -11,10 +10,31 @@ public class ExponentialFunction extends Function {
 	public void createThroughPoints(Point p, Point q) {
 		double a = Math.pow(q.getY() / p.getY(), 1 / (q.getX() - p.getX()));
 		double b = p.getY() / Math.pow(a, p.getX());
-		
-		 expression = Function.roundDouble(b, 3) + " * " + Function.roundDouble(a, 3) + "^x";
+
+
+		if ( a == 0 || b == 0)
+			expression = "0";
+		else {
+
+			if (b - (int) b == 0)
+				expression += (b != 1.0) ? (int) b : ""; 
+			else
+				expression += (b != 1.0) ? Function.roundDouble(b, 3) : "";
+
+			if (!expression.equals("") && a!= 1.0)
+				expression += " * ";
+				
+			if (a - (int) a == 0)
+				expression += (a != 1.0) ? (int) a + "^x": "";
+			else
+				expression += (a != 1.0) ? Function.roundDouble(a, 3) + "^x": "";
+			
+			
+			if (expression.equals(""));
+				expression = "1";
+
+		}
+
 	}
 
-	
-	
 }
--- Mathe/src/willigrossBubble/LinearFunction.java
+++ Mathe/src/willigrossBubble/LinearFunction.java
@@ -9,10 +9,29 @@ public class LinearFunction extends Function {
 	 */
 	public void createThroughPoints(Point p, Point q) {
 		double m = (q.getY() - p.getY()) / (q.getX() - p.getX());
+		
+		if (Double.isNaN(m))
+			m = 0;
+		
 		double t = p.getY() - m * p.getX();
 		
-		 expression = Function.roundDouble(m, 3) + " * x + " + Function.roundDouble(t, 3);
+		
+		if ( m != 0) {
+			if (m - (int) m == 0)
+				expression += (m != 1.0) ? (int) m + " * x" : "x"; 
+			else
+				expression += (m != 1.0) ? Function.roundDouble(m, 3) + " * x" : "x";
+		}
+		
+		if (!expression.equals("") && t != 0)
+			expression += " + ";
+			
+		if (t - (int) t == 0)
+			expression += (t != 0.0) ? (int) t : "";
+		else
+			expression += (t != 0.0) ? Function.roundDouble(t, 3) : "";
+
 	}
-	
-	
+
+
 }
