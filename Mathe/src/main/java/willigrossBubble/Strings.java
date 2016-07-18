565f2d38c4726f227bc8c3383b81af912e64607f
WilliGross <dergrossewilligross@gmail.com>
2016-07-18 17:24:35 +0200

    Organize sources by separating java classes and resources

--- Mathe/src/main/java/willigrossBubble/Strings.java
+++ Mathe/src/main/java/willigrossBubble/Strings.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Strings {
 	
-	private static final String			BUNDLE_NAME		= "willigrossBubble.strings";			//$NON-NLS-1$
+	private static final String			BUNDLE_NAME		= "assets.lang.strings";			//$NON-NLS-1$
 	
 	private static final ResourceBundle	RESOURCE_BUNDLE	= ResourceBundle.getBundle(BUNDLE_NAME);
 	
